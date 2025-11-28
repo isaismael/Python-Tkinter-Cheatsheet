f22402d0aad65c0e94657c453429114c6f1a2b9f
Ismael Isa <ismael.isa02@example.com>
2025-11-27 21:23:00 -0300

    add checkbox

--- cap4/main.py
+++ cap4/main.py
@@ -1,16 +1,28 @@
-import tkinter as tk;
+import tkinter as tk
 
-root = tk.Tk();
-root.geometry("450x250");
-root.title("Tkinter Text");
+root = tk.Tk()
+root.geometry("450x250")
+root.title("Tkinter Text")
 
-text_widget = tk.Text(root, height=10, width=50);
-text_widget.pack();
+is_checked = tk.IntVar();
+
+check = tk.Checkbutton(root, text="Acepto", variable=is_checked)
+check.pack()
+
+text_widget = tk.Text(root, height=10, width=50)
+text_widget.pack()
+
 
 def get_text():
-    text = text_widget.get("1.0", tk.END);
+    text = text_widget.get("1.0", tk.END)
+    check_var = is_checked.get()
+    if check_var == 1:
+        print("el check esta presionado")
+    else:
+        print("El check no está presionado")
     print(text)
 
-tk.Button(root,text="click" ,command=get_text).pack();
 
-root.mainloop();
+tk.Button(root, text="click", command=get_text).pack()
+
+root.mainloop()
